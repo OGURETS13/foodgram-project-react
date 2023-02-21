752e8d3a0420331d800f1cc5e1ca553e591dcdc0
Михаил Францкевич <ogurets13@gmail.com>
2023-02-21 10:56:40 +0300

    debug off

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8jh_3pojmi-&_^_q5qa@(r89fz+^o646yhy9(i&s4^-ux6hfhp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = [
